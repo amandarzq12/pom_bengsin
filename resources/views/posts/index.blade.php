ea18e5383365707e5ee4cf61fefd6c2a42fdacbb
amandarzq12 <amanda.rzq12@gmail.com>
2022-09-23 20:51:56 +0700

    Bengsin

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,57 @@
+@extends('welcome')
+@section('content')
+@section('page', 'Data Pembelian')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card border-0 shadow rounded">
+                <div class="card-body">
+                    <a href="{{ route('posts.create') }}" class="btn btn-md btn-success mb-3">TAMBAH POST</a>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">GAMBAR</th>
+                                <th scope="col">CATEGORY</th>
+                                <th scope="col">JUDUL</th>
+                                <th scope="col">CONTENT</th>
+                                <th scope="col">AUTHOR</th>
+                                <th scope="col">AKSI</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($posts as $post)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ Storage::url('public/posts/') . $post->image }}" class="rounded"
+                                            style="width: 150px">
+                                    </td>
+                                    <td>{{ $post->category }}</td>
+                                    <td>{{ $post->title }}</td>
+                                    <td>{!! $post->content !!}</td>
+                                    <td>{{ $post->author }}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');"
+                                            action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                            <a href="{{ route('posts.edit', $post->id) }}"
+                                                class="btn btn-sm btn-primary">EDIT</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @empty
+                                <div class="alert alert-danger">
+                                    Data Post belum Tersedia.
+                                </div>
+                            @endforelse
+                        </tbody>
+                    </table>
+                    {{ $posts->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
